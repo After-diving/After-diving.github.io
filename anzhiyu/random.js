502c5104bc47b351b233a7984a6863349c7b18ec
After-diving <yuling.youe@gmail.com>
2025-04-07 16:08:38 +0000

    Site updated: 2025-04-07 16:08:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["62d4f2eb72d6/"];function toRandomPost(){
+var posts=["ace500025b9e/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
